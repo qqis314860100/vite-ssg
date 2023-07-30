8f877b43e730f7149ed9ed5042bab204f414ccbc
qqis314860100 <18876309817@163.com>
2023-07-30 13:57:18 +0800

    feat: 单元测试搭建

---
+++ src/node/__test__/index.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+
+test('add', () => {
+  expect(1 + 1).toBe(2);
+  // 快照
+  expect('map'.slice(1)).toMatchSnapshot();
+  expect('map'.slice(1)).toMatchInlineSnapshot('"ap"');
+});
