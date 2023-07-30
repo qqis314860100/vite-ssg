6c8198f66df140da55720d87f7146092f2dfd6d3
qqis314860100 <18876309817@163.com>
2023-07-30 14:47:05 +0800

    feat: e2e测试

---
+++ scripts/prepare-e2e.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import fse from 'fs-extra';
+import * as execa from 'execa';
+
+const exampleDir = path.resolve(__dirname, '../e2e/playground/basic');
+const ROOT = path.resolve(__dirname, '..');
+
+const defaultOptions = {
+  stdout: process.stdout,
+  stdin: process.stdin,
+  stderr: process.stderr
+};
+
+async function prepareE2E() {
+  if (!fse.existsSync(path.resolve(__dirname, '../dist'))) {
+    // pnpm build
+    execa.commandSync('pnpm build', {
+      cwd: ROOT,
+      ...defaultOptions
+    });
+  }
+
+  // 启动一个无头浏览器
+  execa.commandSync('npx playwright install', {
+    cwd: ROOT,
+    ...defaultOptions
+  });
+
+  execa.commandSync('pnpm dev', {
+    cwd: exampleDir,
+    ...defaultOptions
+  });
+}
+
+prepareE2E();
--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,11 +1,19 @@
 import { createServer } from 'vite';
 import { pluginIndexHtml } from './plugin-island/indexHtml';
 import PluginReact from '@vitejs/plugin-react';
+import { PACKAGE_ROOT } from './constant';
 
 export function createDevServer(root: string) {
   return createServer({
     root,
     // PluginReact 热更新保存react状态的插件
-    plugins: [pluginIndexHtml(), PluginReact()]
+    plugins: [pluginIndexHtml(), PluginReact()],
+    server: {
+      fs: {
+        // 避免启动e2e时,因文件不在e2e目录中而报错The request url "F:/workspace/前端成长之路/vite-ssg/src/runtime/client-entry.tsx" is outside of Vite serving allow list.
+        // 将项目中的所有文件都包含进去
+        allow: [PACKAGE_ROOT]
+      }
+    }
   });
 }
